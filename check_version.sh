0ccd14fd39dac2892f3b56ec553b367ef8edba3e
Tjark Sievers <tjarksievers@icloud.com>
2024-05-10 19:09:27 +0200

    Test check_version script

--- check_version.sh
+++ check_version.sh
@@ -4,7 +4,7 @@ set -e
 poetry config repositories.gitlab "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/pypi"
 pip install quant-met --extra-index-url "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/pypi/simple"
 
-current_version=$(poetry version)
+current_version=$(poetry version | awk '{print $2}')
 registry_version=$(pip show quant-met | grep "Version: " | awk '{print $2}')
 
 echo "$current_version"
