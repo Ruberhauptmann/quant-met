70ac2d3b21b29b97f00605e8f94fe70bf330d063
Tjark Sievers <tjarksievers@icloud.com>
2024-06-01 13:47:37 +0200

    Add checking of version on release branches (#7)
    Add checking of version on release branches

--- check_version.sh
+++ check_version.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-poetry config repositories.gitlab "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/pypi"
-pip install quant-met --extra-index-url "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/pypi/simple"
+pip install quant-met poetry
 
 current_version=$(poetry version | awk '{print $2}')
 registry_version=$(pip show quant-met | grep "Version: " | awk '{print $2}')
 
-if [[ "$current_version" == "$registry_version" ]];
+if [[ "$current_version" != "$registry_version" ]];
 then
-  echo "Version is not bumped!"
+  echo "Version is bumped!"
+  exit 0
+else
   exit 1
 fi
