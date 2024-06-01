f4adf9bc02f6040bcc5f901c05d119300ab5bd36
Tjark Sievers <tjarksievers@icloud.com>
2024-06-01 13:36:22 +0200

    Add checking of version on release branches

--- check_version.sh
+++ check_version.sh
@@ -6,8 +6,10 @@ pip install quant-met poetry
 current_version=$(poetry version | awk '{print $2}')
 registry_version=$(pip show quant-met | grep "Version: " | awk '{print $2}')
 
-if [[ "$current_version" == "$registry_version" ]];
+if [[ "$current_version" != "$registry_version" ]];
 then
-  echo "Version is not bumped!"
+  echo "Version is bumped!"
+  exit 0
+else
   exit 1
 fi
