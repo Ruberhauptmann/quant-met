ddd9600c6e1a964b0a8bf3a5cd3bcf547dbcdba0
Tjark <tjarksievers@icloud.com>
2024-06-10 15:31:11 +0200

    Try different minimizer

--- src/quant_met/minimize_free_energy.py
+++ src/quant_met/minimize_free_energy.py
@@ -13,14 +13,21 @@ def free_energy(
 ) -> float:
     number_k_points = len(k_points)
     bdg_energies, _ = hamiltonian.diagonalize_bdg(k_points, delta_vector)
+    # print(np.sum(np.power(np.abs(delta_vector), 2) / hamiltonian.coloumb_orbital_basis))
+    # print(np.real(np.trace(hamiltonian.hamiltonian_k_space(k_points), axis1=-2, axis2=-1)))
+    # print(bdg_energies)
+    # print(np.sum(bdg_energies, axis=-1))
     k_array: npt.NDArray[np.float64] = (
         np.real(np.trace(hamiltonian.hamiltonian_k_space(k_points), axis1=-2, axis2=-1))
         + np.ones(number_k_points)
         * np.sum(np.power(np.abs(delta_vector), 2) / hamiltonian.coloumb_orbital_basis)
+        # - 0.5 * np.sum(bdg_energies, axis=-1)
         - np.sum(np.log(1 + np.nan_to_num(np.exp(-beta * bdg_energies))), axis=-1)
         / beta
     )
-    integral: float = np.sum(k_array, axis=0) / (2.5980762113533156 * number_k_points)
+    # print(k_array)
+    # print(np.sum(k_array))
+    integral: float = np.sum(k_array) / (2.5980762113533156 * number_k_points)
 
     return integral
 
@@ -28,15 +35,19 @@ def free_energy(
 def minimize_loop(
     beta: float, hamiltonian: BaseHamiltonian, k_points: npt.NDArray[np.float64]
 ) -> optimize.OptimizeResult:
-    initial_guess = np.ones(shape=hamiltonian.number_of_bands)
-    solution = optimize.differential_evolution(
-        free_energy,
+    initial_guess = np.ones(shape=hamiltonian.number_of_bands) * 5
+    solution = optimize.brute(
+        func=free_energy,
         # tol=1e-12,
-        x0=initial_guess,
-        atol=0,
+        # x0=initial_guess,
+        # atol=0,
         # options={"eps": 1e-12, 'ftol': 1e-12, 'gtol': 1e-12},
         args=(beta, hamiltonian, k_points),
-        bounds=[(0, 5) for _ in range(hamiltonian.number_of_bands)],
+        ranges=[(0, 1) for _ in range(hamiltonian.number_of_bands)],
+        Ns=20,
+        workers=10,
+        finish=optimize.fmin,
+        # bounds=[(0, 2) for _ in range(hamiltonian.number_of_bands)],
     )
 
     return solution
