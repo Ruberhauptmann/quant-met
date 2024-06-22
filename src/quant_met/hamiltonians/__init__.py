27d0980c9f7ae9b865a420757d871b38a395acf7
Tjark Sievers <tjarksievers@icloud.com>
2024-06-22 15:16:53 +0200

    Remove old superfluid weight formula

--- src/quant_met/hamiltonians/__init__.py
+++ src/quant_met/hamiltonians/__init__.py
@@ -2,7 +2,6 @@ from ._base_hamiltonian import BaseHamiltonian
 from ._eg_x import EGXHamiltonian
 from ._free_energy import free_energy, free_energy_uniform_pairing
 from ._graphene import GrapheneHamiltonian
-from ._superfluid_weight import calculate_current_op
 
 __all__ = [
     "BaseHamiltonian",
@@ -10,5 +9,4 @@ __all__ = [
     "EGXHamiltonian",
     "free_energy",
     "free_energy_uniform_pairing",
-    "calculate_current_op",
 ]
