99f12c9e4274d3466da68d230e08c409c65421cd
Tjark <tjarksievers@icloud.com>
2024-06-24 16:51:40 +0200

    New free energy formula

--- src/quant_met/hamiltonians/_free_energy.py
+++ src/quant_met/hamiltonians/_free_energy.py
@@ -10,28 +10,18 @@ def free_energy(
     k_points: npt.NDArray[np.float64],
 ) -> float:
     number_k_points = len(k_points)
-    bdg_energies, bdg_vectors = hamiltonian.diagonalize_bdg(k_points, delta_vector)
+    bdg_energies, _ = hamiltonian.diagonalize_bdg(k_points, delta_vector)
 
-    k_array: npt.NDArray[np.float64] = (
-        np.real(np.trace(hamiltonian.hamiltonian_k_space(k_points), axis1=-2, axis2=-1))
-        + np.ones(number_k_points)
-        * np.sum(np.power(np.abs(delta_vector), 2) / hamiltonian.coloumb_orbital_basis)
-        - 0.5
-        * np.array(
-            [
-                np.real(
-                    np.trace(
-                        bdg_vectors[k_index]
-                        @ np.diagflat(np.abs(bdg_energies[k_index]))
-                        @ np.conjugate(bdg_vectors[k_index]).T
-                    )
-                )
-                for k_index in range(number_k_points)
-            ]
-        )
+    k_array = np.array(
+        [
+            np.sum(np.abs(bdg_energies[k_index][0 : hamiltonian.number_of_bands]))
+            for k_index in range(number_k_points)
+        ]
     )
 
-    integral: float = np.sum(k_array, axis=-1) / number_k_points
+    integral: float = -np.sum(k_array, axis=-1) / number_k_points + np.sum(
+        np.power(np.abs(delta_vector), 2) / hamiltonian.coloumb_orbital_basis
+    )
 
     return integral
 
