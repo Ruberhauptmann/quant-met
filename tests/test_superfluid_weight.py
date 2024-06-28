d04af7166288103cc48963c48a617c8df250578c
Tjark Sievers <tjarksievers@icloud.com>
2024-06-28 22:25:01 +0200

    More forgivable tests?

--- tests/test_superfluid_weight.py
+++ tests/test_superfluid_weight.py
@@ -51,7 +51,7 @@ def test_superfluid_weight_egx(ndarrays_regression):
             "D_S_xy": np.array(D_S_xy),
             "D_S_yy": np.array(D_S_yy),
         },
-        default_tolerance=dict(atol=1e-8, rtol=1e-8),
+        default_tolerance=dict(atol=1e-7, rtol=1e-7),
     )
 
 
@@ -98,5 +98,5 @@ def test_superfluid_weight_graphene(ndarrays_regression):
             "D_S_xy": np.array(D_S_xy),
             "D_S_yy": np.array(D_S_yy),
         },
-        default_tolerance=dict(atol=1e-8, rtol=1e-8),
+        default_tolerance=dict(atol=1e-7, rtol=1e-7),
     )
