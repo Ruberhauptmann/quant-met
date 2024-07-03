e5084a128eb05f059c9b8f3b0dd975c610e0c830
Tjark Sievers <tjarksievers@icloud.com>
2024-07-03 17:52:15 +0200

    Fix tests

--- tests/test_superfluid_weight.py
+++ tests/test_superfluid_weight.py
@@ -44,7 +44,7 @@ def test_superfluid_weight_egx(ndarrays_regression):
             "D_S_xy": np.array(D_S_xy),
             "D_S_yy": np.array(D_S_yy),
         },
-        default_tolerance=dict(atol=1e-3, rtol=1e-4),
+        default_tolerance=dict(atol=1e-4, rtol=1e-4),
     )
 
 
