Correct calculation of superfluid weight

src/quant_met/mean_field/quantum_metric.py
@@ -60,7 +60,7 @@ def quantum_metric(
 
 
 def quantum_metric_bdg(
-    h: BaseHamiltonian, k_grid: npt.NDArray[np.float64], band: int
+    h: BaseHamiltonian, k_grid: npt.NDArray[np.float64], bands: list[int]
 ) -> npt.NDArray[np.float64]:
     """Calculate the quantum metric in the BdG state.
 
@@ -85,75 +85,25 @@ def quantum_metric_bdg(
 
     quantum_geom_tensor = np.zeros(shape=(2, 2), dtype=np.complex64)
 
-    for i, direction_1 in enumerate(["x", "y"]):
-        h_derivative_direction_1 = h.bdg_hamiltonian_derivative(k=k_grid, direction=direction_1)
-        for j, direction_2 in enumerate(["x", "y"]):
-            h_derivative_direction_2 = h.bdg_hamiltonian_derivative(k=k_grid, direction=direction_2)
-            for k_index in range(len(k_grid)):
-                for n in [i for i in range(h.number_of_bands) if i != band]:
-                    quantum_geom_tensor[i, j] += (
-                        (
-                            bdg_functions[k_index][:, band].conjugate()
-                            @ h_derivative_direction_1[k_index]
-                            @ bdg_functions[k_index][:, n]
+    for band in bands:
+        for i, direction_1 in enumerate(["x", "y"]):
+            h_derivative_dir_1 = h.bdg_hamiltonian_derivative(k=k_grid, direction=direction_1)
+            for j, direction_2 in enumerate(["x", "y"]):
+                h_derivative_dir_2 = h.bdg_hamiltonian_derivative(k=k_grid, direction=direction_2)
+                for k_index in range(len(k_grid)):
+                    for n in [i for i in range(h.number_of_bands) if i != band]:
+                        quantum_geom_tensor[i, j] += (
+                            (
+                                bdg_functions[k_index][:, band].conjugate()
+                                @ h_derivative_dir_1[k_index]
+                                @ bdg_functions[k_index][:, n]
+                            )
+                            * (
+                                bdg_functions[k_index][:, n].conjugate()
+                                @ h_derivative_dir_2[k_index]
+                                @ bdg_functions[k_index][:, band]
+                            )
+                            / (energies[k_index][band] - energies[k_index][n]) ** 2
                         )
-                        * (
-                            bdg_functions[k_index][:, n].conjugate()
-                            @ h_derivative_direction_2[k_index]
-                            @ bdg_functions[k_index][:, band]
-                        )
-                        / (energies[k_index][band] - energies[k_index][n]) ** 2
-                    )
-
-    return np.real(quantum_geom_tensor) / number_k_points
-
-
-def quantum_metric_bdg_occ(
-    h: BaseHamiltonian, k_grid: npt.NDArray[np.float64], band: int
-) -> npt.NDArray[np.float64]:
-    """Calculate the quantum metric in the BdG state.
-
-    Parameters
-    ----------
-    h : :class:`~quant_met.BaseHamiltonian`
-        Hamiltonian object.
-    k_grid : :class:`numpy.ndarray`
-        List of k points.
-    band : int
-        Index of band for which the quantum metric is calculated.
-
-    Returns
-    -------
-    :class:`numpy.ndarray`
-        Quantum metric in the normal state.
-
-    """
-    energies, bdg_functions = h.diagonalize_bdg(k_grid)
-
-    number_k_points = len(k_grid)
-
-    quantum_geom_tensor = np.zeros(shape=(2, 2), dtype=np.complex64)
-
-    for i, direction_1 in enumerate(["x", "y"]):
-        h_derivative_direction_1 = h.bdg_hamiltonian_derivative(k=k_grid, direction=direction_1)
-        for j, direction_2 in enumerate(["x", "y"]):
-            h_derivative_direction_2 = h.bdg_hamiltonian_derivative(k=k_grid, direction=direction_2)
-            for k_index in range(len(k_grid)):
-                for n in [
-                    i for i in range(h.number_of_bands) if i != band and energies[k_index][i] < 0
-                ]:
-                    quantum_geom_tensor[i, j] += (
-                        (
-                            bdg_functions[k_index][:, band].conjugate()
-                            @ h_derivative_direction_1[k_index]
-                            @ bdg_functions[k_index][:, n]
-                        )
-                        * (
-                            bdg_functions[k_index][:, n].conjugate()
-                            @ h_derivative_direction_2[k_index]
-                            @ bdg_functions[k_index][:, band]
-                        )
-                        / (energies[k_index][band] - energies[k_index][n]) ** 2
-                    )
 
     return np.real(quantum_geom_tensor) / number_k_points

src/quant_met/mean_field/superfluid_weight.py
@@ -71,34 +71,8 @@ def _current_operator(
     return j
 
 
-def _w_matrix(
-    h: BaseHamiltonian, k: npt.NDArray[np.float64]
-) -> tuple[npt.NDArray[np.complex64], npt.NDArray[np.complex64]]:
-    _, bloch = h.diagonalize_nonint(k=k)
-    _, bdg_functions = h.diagonalize_bdg(k=k)
-
-    w_plus = np.zeros((2 * h.number_of_bands, h.number_of_bands), dtype=np.complex64)
-    for i in range(2 * h.number_of_bands):
-        for m in range(h.number_of_bands):
-            w_plus[i, m] = (
-                np.tensordot(bloch[:, m], np.array([1, 0]), axes=0).reshape(-1)
-                @ bdg_functions[:, i]
-            )
-
-    w_minus = np.zeros((2 * h.number_of_bands, h.number_of_bands), dtype=np.complex64)
-    for i in range(2 * h.number_of_bands):
-        for m in range(h.number_of_bands):
-            w_minus[i, m] = (
-                np.tensordot(bloch[:, m].conjugate(), np.array([0, 1]), axes=0).reshape(-1)
-                @ bdg_functions[:, i]
-            )
-
-    return w_plus, w_minus
-
-
 def _c_factor(h: BaseHamiltonian, k: npt.NDArray[np.float64]) -> npt.NDArray[np.complex64]:
-    bdg_energies, _ = h.diagonalize_bdg(k)
-    w_plus, w_minus = _w_matrix(h, k)
+    bdg_energies, bdg_functions = h.diagonalize_bdg(k)
     c_mnpq = np.zeros(
         shape=(
             h.number_of_bands,
@@ -124,10 +98,10 @@ def _c_factor(h: BaseHamiltonian, k: npt.NDArray[np.float64]) -> npt.NDArray[np.
                                 c_tmp -= _fermi_dirac_derivative()
 
                             c_tmp *= (
-                                w_minus[i, m].conjugate()
-                                * w_plus[j, n]
-                                * w_minus[j, p].conjugate()
-                                * w_minus[i, q]
+                                bdg_functions[i, m].conjugate()
+                                * bdg_functions[j, n]
+                                * bdg_functions[j, p].conjugate()
+                                * bdg_functions[i, q].conjugate()
                             )
 
                     c_mnpq[m, n, p, q] = 2 * c_tmp