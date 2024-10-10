2d5b8b3f32937c4072fbe58e68465dc2325bd40f
Tjark Sievers <tjarksievers@icloud.com>
2024-10-10 09:12:33 +0200

    Version file for testing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,9 +37,9 @@ extensions = [
 
 intersphinx_mapping = {
     "h5py": ("https://docs.h5py.org/en/latest", None),
-    "matplotlib": ("https://matplotlib.org/stable", None),
-    "numpy": ("https://numpy.org/doc/stable", None),
-    "pandas": ("https://pandas.pydata.org/docs", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
 }
 
 autodoc_typehints = "none"
