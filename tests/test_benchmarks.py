d5da45adeb5772e0c2cc43370295944bda827073
Tjark Sievers <tjarksievers@icloud.com>
2025-01-09 10:54:50 +0100

    One more benchmark

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -7,24 +7,19 @@ import numpy as np
 
 
 @pytest.mark.benchmark()
-def test_benchmark_self_consistency_dressed_graphene() -> None:
-    """Benchmark self-consistency for dressed Graphene."""
-    graphene_lattice = geometry.GrapheneLattice(lattice_constant=np.sqrt(3))
-
-    dressed_graphene_h = mean_field.hamiltonians.DressedGraphene(
-        parameters=parameters.DressedGrapheneParameters(
-            hopping_gr=1,
-            hopping_x=0.01,
-            hopping_x_gr_a=1,
-            lattice_constant=graphene_lattice.lattice_constant,
+def test_benchmark_self_consistency_one_band() -> None:
+    """Benchmark self-consistency for the one band model."""
+    one_band_h = mean_field.hamiltonians.OneBand(
+        parameters=parameters.OneBandParameters(
+            hopping=1,
+            lattice_constant=1,
             chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0, 1.0, 1.0],
+            hubbard_int_orbital_basis=[1.0],
         )
     )
 
     mean_field.self_consistency_loop(
-        h=dressed_graphene_h,
-        k_space_grid=graphene_lattice.generate_bz_grid(40, 40),
-        epsilon=1e-2,
-        max_iter=1000,
+        h=one_band_h,
+        k_space_grid=one_band_h.lattice.generate_bz_grid(ncols=40, nrows=40),
+        epsilon=1e-3,
     )
