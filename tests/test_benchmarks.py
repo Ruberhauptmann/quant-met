a08cc83ae927280895762fa0fedbdbb43e16a664
Tjark Sievers <tjarksievers@icloud.com>
2025-01-09 11:17:03 +0100

    Fix unit tests

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -2,8 +2,10 @@
 #
 # SPDX-License-Identifier: MIT
 from quant_met import mean_field, parameters
+import pytest
 
 
+@pytest.mark.slow_integration_test
 def test_benchmark_self_consistency_one_band(benchmark) -> None:
     """Benchmark self-consistency for the one band model."""
     one_band_h = mean_field.hamiltonians.OneBand(
@@ -16,7 +18,7 @@ def test_benchmark_self_consistency_one_band(benchmark) -> None:
     )
     k_space_grid = one_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
 
-    benchmark(mean_field.self_consistency_loop(
+    benchmark(lambda: mean_field.self_consistency_loop(
         h=one_band_h,
         k_space_grid=k_space_grid,
         epsilon=1e-2,
