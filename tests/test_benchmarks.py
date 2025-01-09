2d208d0ca42381ee1364ea9f29ae772cd3d066fc
Tjark Sievers <tjarksievers@icloud.com>
2025-01-09 21:09:39 +0100

    Change benchmarks

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 @pytest.mark.slow_integration_test
-def test_benchmark_self_consistency_one_band(benchmark) -> None:
+def test_benchmark_gap_equation(benchmark) -> None:
     """Benchmark self-consistency for the one band model."""
     one_band_h = mean_field.hamiltonians.OneBand(
         parameters=parameters.OneBandParameters(
@@ -19,30 +19,6 @@ def test_benchmark_self_consistency_one_band(benchmark) -> None:
     )
     k_space_grid = one_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
 
-    benchmark(lambda: mean_field.self_consistency_loop(
-        h=one_band_h,
-        k_space_grid=k_space_grid,
-        delta_init=np.array([1], dtype=np.complex64),
-        epsilon=1e-2,
-    ))
-
-
-@pytest.mark.slow_integration_test
-def test_benchmark_self_consistency_two_band(benchmark) -> None:
-    """Benchmark self-consistency for the two band model."""
-    one_band_h = mean_field.hamiltonians.TwoBand(
-        parameters=parameters.TwoBandParameters(
-            hopping=1,
-            lattice_constant=1,
-            chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0, 1.0],
-        )
+    benchmark(lambda: one_band_h.gap_equation(
+        k=k_space_grid)
     )
-    k_space_grid = one_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
-
-    benchmark(lambda: mean_field.self_consistency_loop(
-        h=one_band_h,
-        k_space_grid=k_space_grid,
-        delta_init=np.array([1, 1], dtype=np.complex64),
-        epsilon=1e-2,
-    ))
