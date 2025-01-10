701906b4346caf22915b83ea7214b57fa9a5d25b
Tjark Sievers <tjarksievers@icloud.com>
2025-01-10 07:34:07 +0100

    More benchmarks

--- tests/test_benchmarks/test_current.py
+++ tests/test_benchmarks/test_current.py
@@ -8,63 +8,6 @@ import pytest
 
 
 @pytest.mark.slow_integration_test
-def test_benchmark_current_one_band(benchmark) -> None:
-    """Benchmark current for the one band model."""
-    one_band_h = mean_field.hamiltonians.OneBand(
-        parameters=parameters.OneBandParameters(
-            hopping=1,
-            lattice_constant=1,
-            chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0],
-            delta=np.array([1.0], dtype=np.complex128)
-        )
-    )
-    k_space_grid = one_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
-
-    benchmark(lambda: one_band_h.calculate_current_density(
-        k=k_space_grid)
-    )
-
-
-@pytest.mark.slow_integration_test
-def test_benchmark_current_two_band(benchmark) -> None:
-    """Benchmark current for the two band model."""
-    two_band_h = mean_field.hamiltonians.TwoBand(
-        parameters=parameters.TwoBandParameters(
-            hopping=1,
-            lattice_constant=1,
-            chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0, 1.0],
-            delta=np.array([1.0, 1.0], dtype=np.complex128)
-        )
-    )
-    k_space_grid = two_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
-
-    benchmark(lambda: two_band_h.calculate_current_density(
-        k=k_space_grid)
-    )
-
-
-@pytest.mark.slow_integration_test
-def test_benchmark_current_three_band(benchmark) -> None:
-    """Benchmark current for the three band model."""
-    three_band_h = mean_field.hamiltonians.ThreeBand(
-        parameters=parameters.ThreeBandParameters(
-            hopping=1,
-            lattice_constant=1,
-            chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0, 1.0, 1.0],
-            delta=np.array([1.0, 1.0, 1.0], dtype=np.complex128)
-        )
-    )
-    k_space_grid = three_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
-
-    benchmark(lambda: three_band_h.calculate_current_density(
-        k=k_space_grid)
-    )
-
-
-@pytest.mark.slow_integration_test
 def test_benchmark_current_dressed_graphene(benchmark) -> None:
     """Benchmark self-consistency for the dressed graphene model."""
     dressed_graphene_h = mean_field.hamiltonians.DressedGraphene(
--- tests/test_benchmarks/test_superfluid_weight.py
+++ tests/test_benchmarks/test_superfluid_weight.py
@@ -2,69 +2,8 @@
 #
 # SPDX-License-Identifier: MIT
 import numpy as np
-
 from quant_met import mean_field, parameters
 import pytest
-
-
-@pytest.mark.slow_integration_test
-def test_benchmark_superfluid_weight_one_band(benchmark) -> None:
-    """Benchmark superfluid weight for the one band model."""
-    one_band_h = mean_field.hamiltonians.OneBand(
-        parameters=parameters.OneBandParameters(
-            hopping=1,
-            lattice_constant=1,
-            chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0],
-            delta=np.array([1.0], dtype=np.complex128)
-        )
-    )
-    k_space_grid = one_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
-
-    benchmark(lambda: mean_field.superfluid_weight(
-        h=one_band_h,
-        k=k_space_grid)
-    )
-
-
-@pytest.mark.slow_integration_test
-def test_benchmark_superfluid_weight_two_band(benchmark) -> None:
-    """Benchmark superfluid weight for the two band model."""
-    two_band_h = mean_field.hamiltonians.TwoBand(
-        parameters=parameters.TwoBandParameters(
-            hopping=1,
-            lattice_constant=1,
-            chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0, 1.0],
-            delta=np.array([1.0, 1.0], dtype=np.complex128)
-        )
-    )
-    k_space_grid = two_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
-
-    benchmark(lambda: mean_field.superfluid_weight(
-        h=two_band_h,
-        k=k_space_grid)
-    )
-
-
-@pytest.mark.slow_integration_test
-def test_benchmark_superfluid_weight_three_band(benchmark) -> None:
-    """Benchmark superfluid weight for the three band model."""
-    three_band_h = mean_field.hamiltonians.ThreeBand(
-        parameters=parameters.ThreeBandParameters(
-            hopping=1,
-            lattice_constant=1,
-            chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0, 1.0, 1.0],
-            delta=np.array([1.0, 1.0, 1.0], dtype=np.complex128)
-        )
-    )
-    k_space_grid = three_band_h.lattice.generate_bz_grid(ncols=30, nrows=30)
-
-    benchmark(lambda: mean_field.superfluid_weight(
-        h=three_band_h,
-        k=k_space_grid)
-    )
 
 
 @pytest.mark.slow_integration_test
