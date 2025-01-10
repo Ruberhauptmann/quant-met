8eb0f2d3e0eeee518563fae425243c0311feac17
Tjark Sievers <tjarksievers@icloud.com>
2025-01-10 07:47:45 +0100

    More benchmarks

--- tests/test_benchmarks/test_superfluid_weight.py
+++ tests/test_benchmarks/test_superfluid_weight.py
@@ -7,22 +7,20 @@ import pytest
 
 
 @pytest.mark.slow_integration_test
-def test_benchmark_superfluid_weight_dressed_graphene(benchmark) -> None:
+def test_benchmark_superfluid_weight_one_band(benchmark) -> None:
     """Benchmark superfluid weight for the dressed graphene model."""
-    dressed_graphene_h = mean_field.hamiltonians.DressedGraphene(
-        parameters=parameters.DressedGrapheneParameters(
-            hopping_gr=1,
-            hopping_x=0.01,
-            hopping_x_gr_a=1,
+    one_band_h = mean_field.hamiltonians.OneBand(
+        parameters=parameters.OneBandParameters(
+            hopping=1,
             lattice_constant=1,
             chemical_potential=0,
-            hubbard_int_orbital_basis=[1.0, 1.0, 1.0],
-            delta=np.array([1.0, 1.0, 1.0], dtype=np.complex128)
+            hubbard_int_orbital_basis=[1.0],
+            delta=np.array([1.0], dtype=np.complex128)
         )
     )
-    k_space_grid = dressed_graphene_h.lattice.generate_bz_grid(ncols=10, nrows=10)
+    k_space_grid = one_band_h.lattice.generate_bz_grid(ncols=10, nrows=10)
 
     benchmark(lambda: mean_field.superfluid_weight(
-        h=dressed_graphene_h,
+        h=one_band_h,
         k=k_space_grid)
     )
