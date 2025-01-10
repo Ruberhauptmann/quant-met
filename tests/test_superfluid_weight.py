587ae78534cd22dec36fd400705696aaa56781c6
Tjark Sievers <tjarksievers@icloud.com>
2025-01-10 16:44:41 +0100

    Fix tests

--- tests/test_superfluid_weight.py
+++ tests/test_superfluid_weight.py
@@ -38,7 +38,7 @@ def test_superfluid_weight_dressed_graphene(ndarrays_regression: NDArraysRegress
             "D_S_conv": np.array(d_s_conv),
             "D_S_geom": np.array(d_s_geom),
         },
-        default_tolerance={"atol": 1e-7, "rtol": 1e-5},
+        default_tolerance={"atol": 1e-6, "rtol": 1e-5},
     )
 
 
