a0e49f66d45e572b291dc90ca8d367d441681ffb
Tjark Sievers <tjarksievers@icloud.com>
2025-03-07 16:12:03 +0100

    Fix SF weight

--- src/quant_met/mean_field/__init__.py
+++ src/quant_met/mean_field/__init__.py
@@ -29,13 +29,11 @@ Functions
 
 from quant_met.mean_field import hamiltonians
 
-from .quantum_metric import quantum_metric
 from .search_crit_temp import search_crit_temp
 from .self_consistency import self_consistency_loop
 
 __all__ = [
     "hamiltonians",
-    "quantum_metric",
     "search_crit_temp",
     "self_consistency_loop",
 ]
