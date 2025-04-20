7960b42dd9d2f5022bc36615d0b59c8a16f66ff0
Tjark Sievers <tjarksievers@icloud.com>
2025-04-20 15:09:40 +0200

    Cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import importlib.metadata
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../src/quant_met'))
 
 project = "quant-met"
 copyright = "2024, Tjark Sievers"
