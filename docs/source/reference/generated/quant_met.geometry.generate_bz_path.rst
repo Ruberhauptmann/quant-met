7878df862ed8ce01c056c2e7a81288e1d6f433e0
Tjark Sievers <tjarksievers@icloud.com>
2025-04-20 12:25:59 +0200

    Cleanup

--- docs/source/reference/generated/quant_met.geometry.generate_bz_path.rst
+++ docs/source/reference/generated/quant_met.geometry.generate_bz_path.rst
@@ -1,5 +1,4 @@
 ﻿.. SPDX-FileCopyrightText: 2024 Tjark Sievers
-
 .. SPDX-FileCopyrightText: 2025 Tjark Sievers
 ..
 .. SPDX-License-Identifier: MIT
--- src/quant_met/__init__.py
+++ src/quant_met/__init__.py
@@ -4,3 +4,7 @@
 # SPDX-License-Identifier: MIT
 
 """quant-met, a package to treat superconductivity in flat-band systems."""
+
+from . import cli, geometry, mean_field, parameters, plotting
+
+__all__ = ["cli", "geometry", "mean_field", "parameters", "plotting"]
