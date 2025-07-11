408b409e6edce2697011762d667f8cbf47972bb0
Tjark Sievers <tjarksievers@icloud.com>
2025-07-11 20:31:16 +0200

    Implement crit-temp and test for Graphene

--- src/quant_met/routines/__init__.py
+++ src/quant_met/routines/__init__.py
@@ -8,6 +8,7 @@ Routines
     self_consistency_loop
 """  # noqa: D205, D400
 
+from .search_crit_temp import search_crit_temp
 from .self_consistency import self_consistency_loop
 
-__all__ = ["self_consistency_loop"]
+__all__ = ["self_consistency_loop", "search_crit_temp"]
