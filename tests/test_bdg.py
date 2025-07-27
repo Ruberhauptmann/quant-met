605365bbf4f226e2e3053b4216b6b8af1bc06266
Tjark Sievers <tjarksievers@icloud.com>
2025-07-27 06:47:26 +0200

    SCF test

--- tests/test_bdg.py
+++ tests/test_bdg.py
@@ -3,7 +3,6 @@ from quant_met.bdg import bdg_hamiltonian
 
 
 def test_bdg_square_lattice(square_lattice_tb):
-    # Test point at Gamma
     k = np.array([0.0, 0.0, 0.0])
 
     delta_0 = 0.2
@@ -14,7 +13,6 @@ def test_bdg_square_lattice(square_lattice_tb):
         delta_orbital_basis=np.array([delta_0])
     )
 
-    # Should return a 2x2 matrix for 1 orbital per site
     assert bdg.shape == (2, 2)
 
     assert np.all(bdg == np.array([[0.+0.j, 0.2-0.j], [0.2+0.j, -0.+0.j]]))
