31d0d093d62b14f23dc179d15e070c818154330c
Tjark Sievers <tjarksievers@icloud.com>
2025-09-13 10:45:55 +0200

    More tests

--- tests/test_bdg.py
+++ tests/test_bdg.py
@@ -1,5 +1,7 @@
 import numpy as np
-from quant_met.bdg import bdg_hamiltonian
+import sisl
+
+from quant_met.bdg import diagonalize_bdg, bdg_hamiltonian, calculate_current_density
 
 
 def test_bdg_square_lattice(square_lattice_tb):
@@ -15,5 +17,26 @@ def test_bdg_square_lattice(square_lattice_tb):
     )
 
     assert bdg.shape == (2, 2)
-
     assert np.all(bdg == np.array([[0.+0.j, 0.2-0.j], [0.2+0.j, -0.+0.j]]))
+
+
+def test_sc_current(square_lattice_tb):
+    k_grid = sisl.MonkhorstPack(square_lattice_tb.geometry, [10, 10, 1])
+
+    delta_0 = 0.2
+
+    bdg_energies, bdg_wavefunctions = diagonalize_bdg(
+        hamiltonian=square_lattice_tb,
+        kgrid=k_grid,
+        delta_orbital_basis=np.array([delta_0]),
+        q=np.array([0.0, 0.0, 0.0])
+    )
+    current = calculate_current_density(
+        hamiltonian=square_lattice_tb,
+        k=k_grid,
+        bdg_energies=bdg_energies,
+        bdg_wavefunctions=bdg_wavefunctions,
+        beta=1000
+    )
+
+    assert np.all(np.equal(current, np.array([0.0, 0.0])))
